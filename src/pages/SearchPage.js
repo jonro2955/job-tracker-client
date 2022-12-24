88eb8a31c251c239998ae0ca52391e8e990295d1
jonro2955 <jonro.2955@gmail.com>
2022-12-23 18:20:35 -0800

    Make search request to server at get route: "/api/get/search-terms"

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,19 +3,40 @@ import Context from "../utils/context";
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { FormGroup, Label, FormText } from "reactstrap";
+import { FormGroup, Label } from "reactstrap";
 import { DatePicker } from "reactstrap-date-picker";
+import SearchRadio from "../components/SearchRadio";
 
 export default function SearchPage() {
   const context = useContext(Context);
-  const [appsList, setAppsList] = useState([]);
+  const [displayedAppsList, setDisplayedAppsList] = useState([]);
+  const [searchString, setSearchString] = useState("");
   const [selectedSearchOption, setSelectedSearchOption] = useState("option1");
-
-  const [dateRangeStart, setDateRangeStart] = useState(new Date().toISOString());
+  const [dateRangeStart, setDateRangeStart] = useState("");
   const [fmtRangeStart, setFmtRangeStart] = useState(undefined);
-
-  const [dateRangeEnd, setDateRangeEnd] = useState(new Date().toISOString());
+  const [dateRangeEnd, setDateRangeEnd] = useState("");
   const [fmtRangeEnd, setFmtRangeEnd] = useState(undefined);
+
+  useEffect(() => {
+    // when this component mounts, if user is logged in, retrieve user's applications list from db and store it inside displayedAppsList
+    if (context.isAuthenticated && context.dbProfileState) {
+      const username = context.dbProfileState.username;
+      axios
+        .get("/api/get/all-user-records", { params: { email: username } })
+        .then((res) => {
+          setDisplayedAppsList(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [context]);
+
+  useEffect(() => {
+    // when the date range changes, retrieve the corresponding records and load it into displayedAppsList using setDisplayedAppsList()
+    if (fmtRangeStart || fmtRangeEnd) {
+      console.log(`Range start: ${fmtRangeStart}. Range end: ${fmtRangeEnd}`);
+    }
+  }, [fmtRangeStart, fmtRangeEnd]);
+
 
   function handleSetRangeStart(value, formattedValue) {
     setDateRangeStart(value);
@@ -27,103 +48,70 @@ export default function SearchPage() {
     setFmtRangeEnd(formattedValue);
   }
 
-  useEffect(() => {
-    console.log(selectedSearchOption);
-  }, [selectedSearchOption]);
-
-  useEffect(() => {
-    console.log(`Formatted start date is ${fmtRangeStart}. Formatted end date is ${fmtRangeEnd}`);
-  }, [fmtRangeStart, fmtRangeEnd]);
-
-  useEffect(() => {
-    // when this component mounts, if user is logged in, retrieve user's applications list from db and store it inside appsList
+  // This gets called when user clicks the search button. Retrieve corresponding applications from db and store it inside displayedAppsList
+  function search(event) {
+    event.preventDefault();
     if (context.isAuthenticated && context.dbProfileState) {
-      let username = context.dbProfileState.username;
+      const username = context.dbProfileState.username;
+      const inputStr = event.target.querySelector("#searchInput").value;
       axios
-        .get("/api/get/apps", { params: { email: username } })
+        .get("/api/get/search-terms", {
+          params: { email: username, searchStr: inputStr, searchOption: selectedSearchOption },
+        })
         .then((res) => {
-          // console.log(res.data);
-          setAppsList(res.data);
+          setDisplayedAppsList(res.data);
+          console.log(res.data);
         })
         .catch((err) => console.log(err));
     }
-  }, [context]);
+  }
 
   return (
     <div className="centeredPage">
       <h1>Search</h1>
-      <form className="form-inline d-flex my-2 my-lg-0">
+      <form className="form-inline d-flex my-2 my-lg-0" onSubmit={search}>
         <input
           className="form-control mr-sm-2"
+          id="searchInput"
           type="search"
           placeholder="Search"
           aria-label="Search"
+          value={searchString}
+          onChange={(e) => {
+            setSearchString(e.target.value);
+          }}
         />
         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
           Search
         </button>
       </form>
 
-      <strong>Criteria:</strong>
+      <SearchRadio
+        selectedSearchOption={selectedSearchOption}
+        setSelectedSearchOption={setSelectedSearchOption}
+      />
 
-      <form>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="react-tips"
-              value="option1"
-              checked={selectedSearchOption === "option1"}
-              onClick={() => {
-                setSelectedSearchOption("option1");
-              }}
-              className="form-check-input"
-              readOnly
-            />
-            Company
-          </label>
-        </div>
+      <div className="d-flex mt-4">
+        <FormGroup>
+          <Label>Date Range Start</Label>
+          <DatePicker
+            id="example-datepicker1"
+            value={dateRangeStart}
+            onChange={(v, f) => handleSetRangeStart(v, f)}
+          />
+        </FormGroup>
+        &nbsp;
+        <FormGroup>
+          <Label>Date Range End</Label>
+          <DatePicker
+            id="example-datepicker2"
+            value={dateRangeEnd}
+            onChange={(v, f) => handleSetRangeEnd(v, f)}
+          />
+        </FormGroup>
+      </div>
 
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              name="react-tips"
-              value="option2"
-              checked={selectedSearchOption === "option2"}
-              onClick={() => {
-                setSelectedSearchOption("option2");
-              }}
-              className="form-check-input"
-              readOnly
-            />
-            Job Title
-          </label>
-        </div>
-      </form>
-
-      <strong>Select date range</strong>
-      <FormGroup>
-        <Label>Range Start</Label>
-        <DatePicker
-          id="example-datepicker"
-          value={dateRangeStart}
-          onChange={(v, f) => handleSetRangeStart(v, f)}
-        />
-        <FormText>Help</FormText>
-      </FormGroup>
-
-      <FormGroup>
-        <Label>Range End</Label>
-        <DatePicker
-          id="example-datepicker"
-          value={dateRangeEnd}
-          onChange={(v, f) => handleSetRangeEnd(v, f)}
-        />
-        <FormText>Help</FormText>
-      </FormGroup>
-
-      <table className="table table-bordered text-center w-50">
+      <table className="table table-bordered text-center w-50 mt-4">
         <thead>
           <tr>
             <th>Date</th>
@@ -133,8 +121,8 @@ export default function SearchPage() {
           </tr>
         </thead>
         <tbody>
-          {appsList.length > 0 &&
-            appsList.map((item, i) => {
+          {displayedAppsList.length > 0 &&
+            displayedAppsList.map((item, i) => {
               let date = new Date(item.application_date);
               return (
                 <tr key={i}>
